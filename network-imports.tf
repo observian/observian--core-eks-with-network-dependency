f4bdaff077db6be62c88ba73fa33e470754bc286
dan <dan.taylor@observian.com>
2021-03-13 19:47:30 -0700

    different tag filters

--- network-imports.tf
+++ network-imports.tf
@@ -1,33 +1,33 @@
 data "aws_vpc" "base-app-vpc" {
     filter {
         name = "tag:Name"
-        values = ["${var.application}-${var.env}-vpc"]
+        values = ["ob-eks-demo-dev-vpc"]
     }
 }
 data "aws_subnet" "base-app-public-alpha" {
     filter {
         name = "tag:Name"
-        values = ["${var.application}-${var.env}-public-alpha"]
+        values = ["*-public-alpha"]
     }
 }
 
 data "aws_subnet" "base-app-public-bravo" {
     filter {
         name = "tag:Name"
-        values = ["${var.application}-${var.env}-public-bravo"]
+        values = ["*-public-bravo"]
     }
 }
 
 data "aws_subnet" "base-app-private-bravo" {
     filter {
         name = "tag:Name"
-        values = ["${var.application}-${var.env}-private-bravo"]
+        values = ["*-private-bravo"]
     }
 }
 
 data "aws_subnet" "base-app-private-alpha" {
     filter {
         name = "tag:Name"
-        values = ["${var.application}-${var.env}-private-alpha"]
+        values = ["*-private-alpha"]
     }
 }
