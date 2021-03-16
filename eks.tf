e086cfd96815145b7a2782e47125f884a264e936
dan <dan.taylor@observian.com>
2021-03-15 19:43:29 -0600

    fewer min nodes

--- eks.tf
+++ eks.tf
@@ -70,9 +70,9 @@ resource "aws_eks_node_group" "spot-eks-node-group" {
   ]
 
   scaling_config {
-    desired_size = 7
+    desired_size = 1
     max_size     = 10
-    min_size     = 7
+    min_size     = 1
   }
   tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}"     = "owned",
